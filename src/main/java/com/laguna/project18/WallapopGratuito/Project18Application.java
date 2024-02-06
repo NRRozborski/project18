7c4dc158db2fd02e576eb465b9f6371b5bdad723
Norbert Rozborski <norbert.rozborski@educa.madrid.org>
2024-02-06 12:51:38 +0100

    Initial commit

---
+++ src/main/java/com/laguna/project18/WallapopGratuito/Project18Application.java
@@ -0,0 +1,13 @@
+package com.laguna.project18.WallapopGratuito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project18Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/laguna/project18/WallapopGratuito/Project18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laguna.project18.WallapopGratuito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
